6a29933a638308a746b86ab2143acae0e3c75b31
Olessia <15834057+olessiabourgart@users.noreply.github.com>
2025-02-25 20:48:28 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import React from 'react'
+
+export default function App() {
+    return <div className="app-root">
+        <h1>Welcome to the Minimalist React Boilerplate!</h1>
+        <p className="instructions">Go to App.jsx to make changes</p>
+    </div>
+}
